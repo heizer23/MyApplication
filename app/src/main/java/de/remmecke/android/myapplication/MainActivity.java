9e6937fd849e386dd096a7fbde2fedb06c3b6c2f
heizer23 <p.remmecke@gmail.com>
2018-01-20 17:43:06 +0100

    Mein erster Commit

--- app/src/main/java/de/remmecke/android/myapplication/MainActivity.java
+++ app/src/main/java/de/remmecke/android/myapplication/MainActivity.java
@@ -2,9 +2,14 @@ package de.remmecke.android.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
